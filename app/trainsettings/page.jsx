b8fe8ac400ed814807ed5431ab11ef9bfc937705
OM3X4 <om3x4e@gmail.com>
2025-02-04 10:07:58 +0200

    responsive test settings

--- app/trainsettings/page.jsx
+++ app/trainsettings/page.jsx
@@ -22,30 +22,30 @@ function TrainChoose() {
     return (
     <>
         <div className='h-[calc(100vh-5rem)] flex items-center justify-center flex-col'>
-            <h1 className='font-Mono text-6xl font-extrabold text-navy mb-24'>Choose Setting You Want</h1>
-            <div>
-                <select name="First Number Digits" id="" className=" text-3xl   mx-5 py-2 px-4 font-semibold rounded-3xl bg-lightNavy text-white" onChange={e => setDigits1(e.target.value)}>
+            <h1 className='font-Mono text-3xl text-center md:text-6xl font-extrabold text-navy mb-14 md:mb-24'>Choose Setting You Want</h1>
+            <div className='flex flex-col items-center justify-center gap-2'>
+                <select name="First Number Digits" id="" className="text-center md:text-3xl max-w-[calc(100vw-10rem)] mx-5 py-2 px-4 font-semibold rounded-3xl bg-lightNavy text-white" onChange={e => setDigits1(e.target.value)}>
                     <option value={1}>1</option>
                     <option value={2}>2</option>
                     <option value={3}>3</option>
                     <option value={4}>4</option>
                     <option value={5}>5</option>
                 </select>
-                <select name="Second Number Digits" id="" className="text-3xl   mx-5 py-2 px-4 font-semibold rounded-3xl bg-lightNavy text-white" onChange={e => setDigits2(e.target.value)}>
+                <select name="Second Number Digits" id="" className="text-center md:text-3xl max-w-[calc(100vw-10rem)]  mx-5 py-2 px-4 font-semibold rounded-3xl bg-lightNavy text-white" onChange={e => setDigits2(e.target.value)}>
                     <option value={1}>1</option>
                     <option value={2}>2</option>
                     <option value={3}>3</option>
                     <option value={4}>4</option>
                     <option value={5}>5</option>
                 </select>
-                <select name="Second Number Digits" id="" className="text-3xl   mx-5 py-2 px-4 font-semibold rounded-3xl bg-lightNavy text-white" onChange={e => setOperation(e.target.value)}>
+                <select name="Second Number Digits" id="" className="text-center md:text-3xl max-w-[calc(100vw-10rem)]  mx-5 py-2 px-4 font-semibold rounded-3xl bg-lightNavy text-white" onChange={e => setOperation(e.target.value)}>
                     <option value={0}>Addition</option>
                     <option value={1}>Subtraction</option>
                     <option value={2}>Multiplication</option>
                     <option value={3}>Square</option>
                     <option value={4}>Square Root</option>
                 </select>
-                <input placeholder='Number Of Questions' name="Number Of Questions" id="" type='number' className="text-3xl   mx-5 py-2 px-8 font-semibold rounded-3xl bg-lightNavy text-white" onChange={e => setNumber(e.target.value)}>
+                <input placeholder='Number Of Questions' name="Number Of Questions" id="" type='number' className="text-center md:text-3xl mx-5 py-2 px-8 font-semibold rounded-3xl bg-lightNavy text-white appearance-none [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none" onChange={e => setNumber(e.target.value)}>
                 </input>
             </div>
             <div className='button' onClick={handleGo}>
